c6c3effba25dc65c285202c83f21d7f8d5220376
刘帅鹏 <shuaipengliu@foxmail.com>
2020-01-08 11:28:26 +0800

    补充提交

--- src/App.vue
+++ src/App.vue
@@ -5,37 +5,37 @@
       <div>
         <el-button size="small" @click="setTagInput1">设置值</el-button>
       </div>
-      <tag-input v-model="tagInputValue1" :tagconfig="tagconfig"> </tag-input>
+      <taginput v-model="tagInputValue1" :tagconfig="tagconfig"> </taginput>
       <div>{{ tagInputValue1 }}</div>
       <el-divider></el-divider>
       <h3>双括号</h3>
-      <tag-input
+      <taginput
         v-model="tagInputValue2"
         :tagconfig="tagconfig"
         :left-symbol="'{{'"
         :right-symbol="'}}'"
       >
-      </tag-input>
+      </taginput>
       <div>{{ tagInputValue2 }}</div>
       <el-divider></el-divider>
       <h3>双括号带校验配置 支持min，max，maxTags</h3>
-      <tag-input
+      <taginput
         v-model="tagInputValue3"
         :tagconfig="tagconfig"
         :left-symbol="'{{'"
         :right-symbol="'}}'"
         :prop="{ min: 6, max: 20, maxTags: 2 }"
       >
-      </tag-input>
+      </taginput>
       <div>{{ tagInputValue3 }}</div>
     </div>
   </div>
 </template>
 <script>
-import tagInput from "./tagInput.vue";
+import taginput from "./taginput.vue";
 export default {
   components: {
-    tagInput
+    taginput
   },
   data() {
     return {
@@ -61,11 +61,6 @@ export default {
           { id: 2, name: "地点" }
         ]
       };
-    }
-  },
-  watch: {
-    tagInputValue1() {
-      console.log("tagInputValue1", this.tagInputValue1);
     }
   }
 };
